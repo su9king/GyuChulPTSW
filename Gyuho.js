587bb0113d76606faafff9175898268813da9a84
shinminchul <su99king@gmail.com>
2024-04-07 23:47:02 +0900

    Initialization

---
+++ Gyuho.js
@@ -0,0 +1,7 @@
+function moveInsta() {
+    location.href = "https://www.instagram.com/gyu_hoo/";
+};
+
+function moveGithub() {
+    location.href = "https://github.com/KU-ARL";
+};
---
+++ Minchul.js
@@ -0,0 +1,7 @@
+function goInsta(){
+    location.href = "https://www.instagram.com/shin.min_chul/";
+}
+
+function goGyuho(){
+    location.href = "Gyuho.html";
+}
