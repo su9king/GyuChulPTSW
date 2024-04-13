de1e3f32f400dc23b71d3400e261da5908e7424b
su9king <su99king@gmail.com>
2024-04-13 21:02:15 +0900

    setting

---
+++ StartPoint/index.js
@@ -0,0 +1,24 @@
+async function login(ID, PW) {
+    // 구글 스프레드시트에서 ID, PW 확인 , 계산 및 알고리즘은 보안상 php언어로 진행.
+    // loginExists : 1 (아이디가 존재하며,비밀번호가 정확함) / : 0 (아이디가 존재하지 않거나, 비밀번호가 틀림)
+    const loginExists = 1; // 예시 값
+
+}
+
+async function register(ID, PW) {
+    // 구글 스프레드시트에서 ID, PW 확인 , 계산 및 알고리즘은 보안상 php언어로 진행.
+    // registerExists : 1 (아이디가 존재함) / : 0 (아이디가 존재하지 않음)
+    const registerExists = 1; // 예시 값
+
+}
+
+async function checkData() {
+    // 구글 스프레드시트에서 모든 ID, PW 데이터 가져오기 계산 및 알고리즘은 보안상 php언어로 진행.
+    const data = [["ID1", "PW1"], ["ID2", "PW2"]]; // 예시 데이터
+    // 최대 데이터 노출 개수는 10개
+
+    // 받아온 데이터를 테이블 형태로 출력하는 코드가 작성되어야 함.
+
+}
+
+// 실행및 계산되는 부분에 대해서는 php언어로 진행함. php 스크립트의 부분에는 index.php 표시 및 추가내용 주석처리 바랍니다.
