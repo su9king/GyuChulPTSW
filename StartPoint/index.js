cf790584fff430e4449d1fae64be2852a9309003
su9king <su99king@gmail.com>
2024-04-17 11:11:42 +0900

    upload01

--- StartPoint/index.js
+++ StartPoint/index.js
@@ -1,24 +1,42 @@
 async function login(ID, PW) {
-    // 구글 스프레드시트에서 ID, PW 확인 , 계산 및 알고리즘은 보안상 php언어로 진행.
-    // loginExists : 1 (아이디가 존재하며,비밀번호가 정확함) / : 0 (아이디가 존재하지 않거나, 비밀번호가 틀림)
-    const loginExists = 1; // 예시 값
+    const functionType = 0;
+    
+    const response = await fetch('index.php', {
+        method : 'POST',
+        headers: {'ContentType' : 'application/x-www-form-urlencoded'},
+        body: `functionType=${functionType}&ID=${ID}&PW=${PW}`
+    });
 
+    const loginExists = await response.text();
+    console.log(loginExists);
+
+    // loginExists : 1 (아이디가 존재하며,비밀번호가 정확함) / : 0 (아이디가 존재하지 않거나, 비밀번호가 틀림)
 }
 
 async function register(ID, PW) {
-    // 구글 스프레드시트에서 ID, PW 확인 , 계산 및 알고리즘은 보안상 php언어로 진행.
+    const functionType = 1;
+    const response = await fetch('index.php', {
+        method : 'POST',
+        headers : {'ContentType' : 'application/x-www-form-urlencoded'},
+        body : `fuctionType=${functionType}&ID=${ID}&PW=${PW}`
+        
+    });
+    
+    const registerExists = await response.text();
+    console.log(registerExists);
     // registerExists : 1 (아이디가 존재함) / : 0 (아이디가 존재하지 않음)
-    const registerExists = 1; // 예시 값
-
 }
 
 async function checkData() {
-    // 구글 스프레드시트에서 모든 ID, PW 데이터 가져오기 계산 및 알고리즘은 보안상 php언어로 진행.
-    const data = [["ID1", "PW1"], ["ID2", "PW2"]]; // 예시 데이터
+    const functionType = 2;
     // 최대 데이터 노출 개수는 10개
+    const respones = await fetch('index.php',{
+        method : 'POST',
+        headers : {'ContentType' : 'application/x-www-form-urlencoded'},
+        body : `functionType=${functionType}&ID=${ID}&PW=${PW}`
+    });
 
-    // 받아온 데이터를 테이블 형태로 출력하는 코드가 작성되어야 함.
-
+    const data = await respones.text();
+    console.log(data);
+   
 }
-
-// 실행및 계산되는 부분에 대해서는 php언어로 진행함. php 스크립트의 부분에는 index.php 표시 및 추가내용 주석처리 바랍니다.
