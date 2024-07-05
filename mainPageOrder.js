91fbfef0820a504edcaf538e35d3a2ce0825a9c9
shinminchul <su99king@gmail.com>
2024-07-05 15:31:56 +0900

    nt7

--- mainPageOrder.js
+++ mainPageOrder.js
@@ -7,14 +7,16 @@ module.exports = { mainPageOrder };
 async function mainPageOrder(functionType,Token) {
 
     if (functionType == 1){ //조직 노출을 위한 사용자가 속한 조직 데이터 제공
-    	console.log(Token);
-        connection.query('SELECT groupID FROM user_groups WHERE userID = ?', [Token],
-            (error, results, fields) => {
-                console.log(results)
-                return results
-        
 
-    })} else if (functionType == 2){
-        //조직 참가를 위한 데이터 제공 or 서버에서 해야할 일.
+        return new Promise((resolve,reject) => {
+            connection.query('SELECT groupID FROM user_groups WHERE userID = ?', [Token],
+                (error, results, fields) => {
+                    console.log(results)
+                    resolve(results);
+                }
+            )
+        })
+    }else if(functionType == 2){
+        console.log("TEST");
     }
 }
