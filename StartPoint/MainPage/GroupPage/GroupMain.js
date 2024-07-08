a0f746d707d26f5d6aa02ca67007ba134ac4dc5a
shinminchul <su99king@gmail.com>
2024-07-09 07:35:42 +0900

    PageTitle

--- StartPoint/MainPage/GroupPage/GroupMain.js
+++ StartPoint/MainPage/GroupPage/GroupMain.js
@@ -1,21 +1,23 @@
-var groupID = sessionStorage.getItem(groupID);
 
-// 버튼 요소 가져오기
-var writeButton = document.getElementById('writeButton');
-var viewButton = document.getElementById('viewButton');
-
-//후에 권한에 따른 리소스 제공 코드 필요
-writeButton.style.display = 'block';
-viewButton.style.display = 'block';
 
 document.addEventListener("DOMContentLoaded", function() {
+
+    var groupID = sessionStorage.getItem('groupID');
+
+    // 버튼 요소 가져오기
+    var writeButton = document.getElementById('writeButton');
+    var viewButton = document.getElementById('viewButton');
+
+    //후에 권한에 따른 리소스 제공 코드 필요
+    writeButton.style.display = 'block';
+    viewButton.style.display = 'block';
     // 서버에서 데이터 가져오기
     fetch(`/mainPageOrder?functionType=2&groupID=${groupID}`) // 실제 API URL로 변경
         .then(response => response.json())
         .then(data => {
             // 데이터 가져오기 성공 시 조직 이름 업데이트
             const organizationNameElement = document.getElementById('organizationName');
-            organizationNameElement.textContent = data['groupName']; // 응답 데이터에서 조직 이름을 가져와 업데이트
+            organizationNameElement.textContent = data[0]['groupName']; // 응답 데이터에서 조직 이름을 가져와 업데이트
         })
         .catch(error => {
             console.error('Error fetching organization name:', error);
