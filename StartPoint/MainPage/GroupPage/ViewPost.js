87afd78f49ca4ed7163b83a843471897ac6853fe
shinminchul <su99king@gmail.com>
2024-07-11 10:30:45 +0900

    sideInterface / process Changed

--- StartPoint/MainPage/GroupPage/ViewPost.js
+++ StartPoint/MainPage/GroupPage/ViewPost.js
@@ -1,9 +1,37 @@
 window.onload = async function() {
-    loadNavbar();
     loadSidebar();
+    loadNavbar();
+
+    var buttonField = document.getElementById('fileList'); // HTML 버튼 추가할 필드 불러오기
+    console.log(buttonField)
+    var writeButton = document.getElementById('writeButton');
+
+    const groupID = sessionStorage.getItem('groupID');
+    const postData = fetch('/postsOrder', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: `functionType=1&groupID=${groupID}`
+    })
+    .then(response => response.json())
+    .then(data => {
+        const postDataArray = data
+        postDataArray.forEach(function(data) {
+            console.log(data)
+            var Item = document.createElement('button');
+            Item.textContent = data['title'];
+            Item.onclick = function() { showContent(data); };
+
+            buttonField.appendChild(Item);
+            });
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        alert('서버와의 통신에 문제가 발생했습니다.');
+    });
 }
 
-writeButton = document.getElementById('writeButton');
 //writeButton.style.display = 'none';
 // const permission = fetch(
 //     ///////////////////////////permission 요청하기///////////////////////////
@@ -12,33 +40,6 @@ writeButton = document.getElementById('writeButton');
 // if (permission == 1) {
 //     writeButton.style.display = 'block';
 // }
-
-var buttonField = document.getElementById('fileList'); // HTML 버튼 추가할 필드 불러오기
-
-const groupID = sessionStorage.getItem('groupID');
-const postData = fetch('/postsOrder', {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/x-www-form-urlencoded'
-    },
-    body: `functionType=1&groupID=${groupID}`
-})
-.then(response => response.json())
-.then(data => {
-    const postDataArray = data
-    postDataArray.forEach(function(data) {
-        console.log(data)
-        var Item = document.createElement('button');
-        Item.textContent = data['title'];
-        Item.onclick = function() { showContent(data); };
-        console.log(Item)
-        buttonField.appendChild(Item);
-        });
-})
-.catch(error => {
-    console.error('Error:', error);
-    alert('서버와의 통신에 문제가 발생했습니다.');
-});
 
 //보여주는 기능 함수이긴한데 실제로 작동하는 원리는
 // title,index,content 값을 쿼리스트링으로 변환하여 Post.html 으로 보내서
